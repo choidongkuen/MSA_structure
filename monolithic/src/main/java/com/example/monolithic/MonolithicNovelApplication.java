b338269c8526bfb086b952440467efef0aeec26e
choidongkuen <danaver12@daum.net>
2023-06-18 23:58:12 +0900

    FEAT : JpaAuditing 관련 작업 구현
    - BaseEntity 에 id,created_at 칼럼 추가

--- monolithic/src/main/java/com/example/monolithic/MonolithicNovelApplication.java
+++ monolithic/src/main/java/com/example/monolithic/MonolithicNovelApplication.java
@@ -3,7 +3,6 @@ package com.example.monolithic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class MonolithicNovelApplication {
 
---
+++ monolithic/src/main/java/com/example/monolithic/domain/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.example.monolithic.domain.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
+
