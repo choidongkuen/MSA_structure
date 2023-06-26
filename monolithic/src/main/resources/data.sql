8307d3e46e52d0737b1e72efe1f4691951696003
choidongkuen <danaver12@daum.net>
2023-06-27 03:03:30 +0900

    FEAT : user-api, payment-api, webbook-api 모듈 추가(msa 구조 구현) 및 spring-cloud open feign 의존성 추가

---
+++ monolithic/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into reader
+values (1, CURRENT_TIMESTAMP, current_timestamp, '최동근', '971201-1216826');
+
+insert into reader
+values (2, CURRENT_TIMESTAMP, current_timestamp, '박건구', '970301-12312321');
+
+
+
+insert into writer
+values (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'danaver12@daum.net', '빙상민');
+
+
+insert into writer
+values (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'whole34@naver.com', '김동언');
